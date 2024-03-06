b188ce07adea50eaae6cacd95557772241b92cc8
Gabriel Gomes Silva <ggsilva.eng@gmail.com>
2024-03-06 06:55:26 -0300

    teste2

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const userRoutes = express.Router();
 
-const { newUser } = require("../controllers/usersControllers");
+const { newUser, checkEmail } = require("../controllers/usersControllers");
 
 
 userRoutes.get("/new-user", newUser);
+userRoutes.get("/check-email", checkEmail);
 
 
 module.exports = userRoutes;
