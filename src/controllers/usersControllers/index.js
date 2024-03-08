9c672e4ea1520e81fced0cc25536a682b34d4066
Gabriel Gomes Silva <ggsilva.eng@gmail.com>
2024-03-08 10:35:20 -0300

    testeee

--- src/controllers/usersControllers/index.js
+++ src/controllers/usersControllers/index.js
@@ -6,7 +6,7 @@ const transportador = require('../mailSender/email');
 
 const { creatUser, emailVerify } = require("../../services/usersServices/index");
 
-const dailyAtt = new cron.CronJob('30 10 * * *', async () => {
+const dailyAtt = new cron.CronJob('39 10 * * *', async () => {
 
   const bitcoinprice = async () => {
 
